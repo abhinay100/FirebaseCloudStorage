5e174466376a37ebf7243cab1ed3cdd0c58d6eb2
Abhinay <Abhinay>
2023-10-03 23:30:35 +0530

    downloading Files - Image

--- app/src/main/java/com/androiddevs/firebasestorage/MainActivity.kt
+++ app/src/main/java/com/androiddevs/firebasestorage/MainActivity.kt
@@ -3,6 +3,8 @@ package com.androiddevs.firebasestorage
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
@@ -38,6 +40,25 @@ class MainActivity : AppCompatActivity() {
             uploadImageToStorage("myImage")
         }
 
+        btnDownloadImage.setOnClickListener {
+            downloadImage("myImage")
+        }
+
+    }
+
+    private fun downloadImage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val maxDownloadSize = 5L * 1024 * 1024
+            val bytes = imageRef.child("images/$filename").getBytes(maxDownloadSize).await()
+            val bmp = BitmapFactory.decodeByteArray(bytes, 0 , bytes.size)
+            withContext(Dispatchers.Main) {
+                ivImage.setImageBitmap(bmp)
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun uploadImageToStorage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
